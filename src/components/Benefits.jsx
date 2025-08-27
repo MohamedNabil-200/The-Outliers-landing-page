97c81e180515c9494ebfd5dba903a75da37a4c92
Mohamed Nabil <mohamed.nabil67999@gmail.com>
2025-08-27 10:32:59 +0300

    fix(build): solve over height border in boxes

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -15,26 +15,26 @@ const Benefits = () => {
           What we offer
         </h3>
         <div className="flex flex-col md:flex-row items-center justify-center md:justify-evenly gap-4 w-full">
-          <div className="border border-[#59B5A6] pt-4 rounded-md mb-4 md:mb-0 w-4/5 md:w-2/5 xl:w-1/5 h-[218px] overflow-hidden" data-aos="fade-right">
+          <div className="border border-[#59B5A6] pt-4 rounded-md mb-4 md:mb-0 w-full md:w-fit h-[218px] overflow-hidden" data-aos="fade-right">
             <h5 className="text-2xl font-extrabold text-[#59B5A6] mb-2 pl-4">
               for Teens:
             </h5>
             <ul className="text-[#212529]">
-              <li className=" bg-[#46af9f] pl-2 pr-4 py-2 text-white font-medium hover:bg-white hover:text-[#46af9f] hover:border-b transition-all duration-300 cursor-default hover:pl-4">Real projects, not boring lectures</li>
-              <li className=" bg-[#92D3C9] pl-2 pr-4 py-2 text-gray-600 font-medium hover:bg-white hover:text-[#46af9f] hover:border-b transition-all duration-300 cursor-default hover:pl-4">AI skill-building</li>
-              <li className=" bg-[#46af9f] pl-2 pr-4 py-2 text-white font-medium hover:bg-white hover:text-[#46af9f] hover:border-b transition-all duration-300 cursor-default hover:pl-4">Early internship access</li>
+              <li className=" bg-[#46af9f] pl-2 pr-4 py-2 text-white font-medium hover:bg-white hover:text-[#46af9f] transition-all duration-300 cursor-default hover:pl-4">Real projects, not boring lectures</li>
+              <li className=" bg-[#92D3C9] pl-2 pr-4 py-2 text-gray-600 font-medium hover:bg-white hover:text-[#46af9f] transition-all duration-300 cursor-default hover:pl-4">AI skill-building</li>
+              <li className=" bg-[#46af9f] pl-2 pr-4 py-2 text-white font-medium hover:bg-white hover:text-[#46af9f] transition-all duration-300 cursor-default hover:pl-4">Early internship access</li>
               <li className=" bg-[#92D3C9] pl-2 pr-4 py-2 text-gray-600 font-medium hover:bg-white hover:text-[#46af9f] transition-all duration-300 cursor-default hover:pl-4">Mentorship and community</li>
             </ul>
           </div>
-          <div className="border border-[#59B5A6] pt-4 rounded-md mb-4 md:mb-0 w-4/5 md:w-2/5 xl:w-1/5 h-[218px] overflow-hidden" data-aos="fade-left">
+          <div className="border border-[#59B5A6] pt-4 rounded-md mb-4 md:mb-0 w-full md:w-fit h-[218px] overflow-hidden" data-aos="fade-left">
             <h5 className="text-2xl font-extrabold text-[#59B5A6] mb-2 pl-4">
               for Startups:
             </h5>
             <ul className="text-[#212529]">
-              <li className=" bg-[#46af9f] pl-2 pr-4 py-2 text-white font-medium hover:bg-white hover:text-[#46af9f] hover:border-b transition-all duration-300 cursor-default hover:pl-4">Access young motivated talent</li>
-              <li className=" bg-[#92D3C9] pl-2 pr-4 py-2 text-gray-600 font-medium hover:bg-white hover:text-[#46af9f] hover:border-b transition-all duration-300 cursor-default hover:pl-4">Contribute to future education</li>
-              <li className=" bg-[#46af9f] pl-2 pr-4 py-2 text-white font-medium hover:bg-white hover:text-[#46af9f] hover:border-b transition-all duration-300 cursor-default hover:pl-4">Offer micro-internships</li>
-              <li className=" bg-[#92D3C9] pl-2 pr-4 py-2 text-gray-600 font-medium hover:bg-white hover:text-[#46af9f] hover:border-b transition-all duration-300 cursor-default hover:pl-4">Test ideas with Gen Z insight</li>
+              <li className=" bg-[#46af9f] pl-2 pr-4 py-2 text-white font-medium hover:bg-white hover:text-[#46af9f] transition-all duration-300 cursor-default hover:pl-4">Access young motivated talent</li>
+              <li className=" bg-[#92D3C9] pl-2 pr-4 py-2 text-gray-600 font-medium hover:bg-white hover:text-[#46af9f] transition-all duration-300 cursor-default hover:pl-4">Contribute to future education</li>
+              <li className=" bg-[#46af9f] pl-2 pr-4 py-2 text-white font-medium hover:bg-white hover:text-[#46af9f] transition-all duration-300 cursor-default hover:pl-4">Offer micro-internships</li>
+              <li className=" bg-[#92D3C9] pl-2 pr-4 py-2 text-gray-600 font-medium hover:bg-white hover:text-[#46af9f] transition-all duration-300 cursor-default hover:pl-4">Test ideas with Gen Z insight</li>
             </ul>
           </div>
         </div>
